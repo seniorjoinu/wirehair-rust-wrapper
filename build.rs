3e3a04bc1a036b10c3bac84535e0d2d50ba62d91
Alexander Vtyurin <senior.joinu@gmail.com>
2019-08-01 18:43:13 +0300

    init project
    add wirehair to cargo build pipeline
    add ffi
    add ffi idiomatic wrapper

---
+++ build.rs
@@ -0,0 +1,16 @@
+extern crate cc;
+
+
+fn main() {
+    cc::Build::new()
+        .cpp(true)
+        .file("src/wirehair/wirehair.cpp")
+        .file("src/wirehair/gf256.cpp")
+        .file("src/wirehair/WirehairCodec.cpp")
+        .file("src/wirehair/WirehairTools.cpp")
+        .include("src/wirehair")
+        .flag("-msse4.1")
+        .out_dir("wirehair")
+        .shared_flag(true)
+        .compile("wirehair");
+}
